b11ce7d9f46e3030852e621991f636c03ab36f65
David Eppenberger <dfen8c@gmail.com>
2015-12-04 10:36:21 -0700

    modified shake to record data instead of looking for a "tap"

--- www/shake.js
+++ www/shake.js
@@ -9,84 +9,32 @@ module.exports = (function () {
         frequency: 1
     };
 
-    var previousAcceleration = {
-        x: null,
-        y: null,
-        z: null
-    };
-
-    var shakeCallBack = null;
-    var sensitivity = 30;
-    var timestamp = null;
+    var data = null;
 
     // Start watching the accelerometer for a shake gesture
-    shake.startWatch = function (onShake, _sensitivity, onError) {
-        if (typeof (onShake) !== "function") {
-            return;
-        }
-
-        if (typeof (_sensitivity) === "number") {
-            sensitivity = _sensitivity;
-        }
-
-        shakeCallBack = debounce(onShake);
+    shake.startWatch = function (onError) {
+    	data = [];
 
         watchId = navigator.accelerometer.watchAcceleration(assessCurrentAcceleration, onError, options);
     };
 
     // Stop watching the accelerometer for a shake gesture
-    shake.stopWatch = function () {
+    shake.stopWatch = function (onStop) {
+    	if (typeof (onStop) !== "function") {
+            return;
+        }
+    	
         if (watchId !== null) {
             navigator.accelerometer.clearWatch(watchId);
             watchId = null;
-
-            previousAcceleration = {
-                x: null,
-                y: null,
-                z: null
-            };
         }
+        
+        onStop(data);
     };
 
     // Assess the current acceleration parameters to determine a shake
-    var assessCurrentAcceleration = function (acceleration) {
-        var accelerationChange = {};
-        if (previousAcceleration.x !== null) {
-            accelerationChange.x = Math.abs(previousAcceleration.x - acceleration.x);
-            accelerationChange.y = Math.abs(previousAcceleration.y - acceleration.y);
-            accelerationChange.z = Math.abs(previousAcceleration.z - acceleration.z);
-        }
-        
-        previousAcceleration = {
-            x: acceleration.x,
-            y: acceleration.y,
-            z: acceleration.z
-        };
-        
-//        console.log("Acceleration: " +JSON.stringify(acceleration) + " Acceleration Change: " + JSON.stringify(accelerationChange) + " Previous Acceleration: " + JSON.stringify(previousAcceleration));
-
-        if (accelerationChange.x + accelerationChange.y + accelerationChange.z > sensitivity) {
-            // Shake detected
-        	timestamp = acceleration.timestamp;
-            shakeCallBack();
-        }
-    };
-
-    // can be used to prevent duplicate shakes within x ms where x is he timeout value
-    var debounce = function (onShake) {
-        var timeout;
-        return function () {
-            if (timeout) {
-                return;
-            }
-
-            timeout = setTimeout(function () {
-                clearTimeout(timeout);
-                timeout = null;
-            }, 50);
-
-            onShake(timestamp);
-        };
+    var assessCurrentAcceleration = function (acceleration) { 
+    	data.push(acceleration);
     };
 
     return shake;
